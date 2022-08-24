b99d674768f504379954264affda0cf045f338f3
mudassirmalik5699 <aminmudassir9@gmail.com>
2022-08-24 19:09:02 +0500

    add category in database

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,11 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    public function view_category(){
+    public function view_category()
+    {
         return view('admin.category');
+    }
+    public function add_category(Request $request)
+    {
+        $data = new Category;
+        $data->category_name = $request->category;
+        $data->save();
+        return back()->with('message', 'Your Category has added in The List!');
     }
 }
