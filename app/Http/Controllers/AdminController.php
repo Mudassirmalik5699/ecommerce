53fb452017a1bdd453757964542ae62c0efb3628
mudassirmalik5699 <aminmudassir9@gmail.com>
2022-08-25 13:57:00 +0500

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,18 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller
+use App\Http\Controllers\Controller;
 class AdminController extends Controller
 {
-    public function view_category()
-    {
-        return view('admin.category');
-    }
-    public function add_category(Request $request)
-    {
-        $data = new Category;
-        $data->category_name = $request->category;
-        $data->save();
-        return back()->with('message', 'Your Category has added in The List!');
-    }
+    // public function view_category()
+    // {
+    //     return view('admin.category');
+    // }
+    // public function add_category(Request $request)
+    // {
+    //     $data = new Category;
+    //     $data->category_name = $request->category;
+    //     $data->save();
+    //     return back()->with('message', 'Your Category has added in The List!');
+    // }
 }
